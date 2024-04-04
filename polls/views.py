b299e3fc7680ad010f448532f51ca35997ccd7f0
Will Krause <wkrause1@radford.edu>
2024-04-04 16:06:06 -0400

    Adjust views

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from polls.models import Question
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
